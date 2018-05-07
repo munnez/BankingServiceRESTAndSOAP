74f909aaee0aabae62f2d592041caf94cfea49a5
mat94 <antosiewicz.mateusz.94@gmail.com>
2018-05-07 11:51:00 +0200

    Initial commit

---
+++ src/main/java/com/example/mateuszantosiewicz/MateuszantosiewiczApplication.java
@@ -0,0 +1,12 @@
+package com.example.mateuszantosiewicz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MateuszantosiewiczApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MateuszantosiewiczApplication.class, args);
+    }
+}
